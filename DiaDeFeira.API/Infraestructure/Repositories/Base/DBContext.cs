34b4f9502c938ebc67145f8b48a0f5ac8df22bfb
Juliane Pires <juliane.pires@lambda3.com.br>
2023-03-13 21:01:52 -0300

    feat: Adiciona métodos que buscam as collections

--- DiaDeFeira.API/Infraestructure/Repositories/Base/DBContext.cs
+++ DiaDeFeira.API/Infraestructure/Repositories/Base/DBContext.cs
@@ -7,6 +7,11 @@ namespace DiaDeFeira.API.Infraestructure.Repositories.Base
     {
         public IMongoClient Client;
         public IMongoCollection<Categoria> Categorias;
+        public IMongoCollection<Produto> Produtos;
+        public IMongoCollection<Item> Itens;
+        public IMongoCollection<Lista> Listas;
+        public IMongoCollection<Usuario> Usuarios;
+        public IMongoCollection<Historico> Historicos;
 
         public DBContext(IConfiguration configuration)
         {
@@ -15,6 +20,16 @@ namespace DiaDeFeira.API.Infraestructure.Repositories.Base
             var database = Client.GetDatabase(configuration["Database:Name"]);
 
             Categorias = database.GetCollection<Categoria>("categorias");
+
+            Produtos = database.GetCollection<Produto>("produtos");
+
+            Itens = database.GetCollection<Item>("itens");
+
+            Listas = database.GetCollection<Lista>("listas");
+
+            Usuarios = database.GetCollection<Usuario>("usuarios");
+
+            Historicos = database.GetCollection<Historico>("historicos");
         }
     }
 }
